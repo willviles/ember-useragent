d2929aa43331f230ca0f85409b9c2b20dbf5c179
Will Viles <will@willviles.com>
2017-03-23 10:49:22 +0000

    Adds blueprint index

---
+++ blueprints/ember-useragent/index.js
@@ -0,0 +1,10 @@
+/*jshint node:true*/
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  }
+};
