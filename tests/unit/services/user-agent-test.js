2c29f4948e72ddc8007a6f9b87546d76f7e8f17e
simon <ihmig@kaliber5.de>
2017-11-17 16:36:25 +0100

    Added some unit tests

---
+++ tests/unit/services/user-agent-test.js
@@ -0,0 +1,60 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:user-agent', 'Unit | Service | user agent', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+const testDevices = [
+  {
+    device: 'iPhone',
+    ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+    assertions: {
+      'browser.isSafari': true,
+      'device.isMobile': true,
+      'os.isIOS': true
+    }
+  },
+  {
+    device: 'iPad',
+    ua: 'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+    assertions: {
+      'browser.isSafari': true,
+      'device.isTablet': true,
+      'os.isIOS': true
+    }
+  },
+  {
+    device: 'Chrome OSX',
+    ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+    assertions: {
+      'browser.isChrome': true,
+      'device.isDesktop': true,
+      'os.isMacOS': true
+    }
+  },
+  {
+    device: 'Galaxy S5',
+    ua: 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36',
+    assertions: {
+      'browser.isChrome': true,
+      'device.isMobile': true,
+      'os.isAndroid': true
+    }
+  }
+];
+
+testDevices.forEach(function(item) {
+  test(item.device, function(assert) {
+    let userAgent = item.ua;
+    let subject = this.subject({
+      userAgent
+    });
+
+    for (let key in item.assertions) {
+      let actual = subject.get(key);
+      let expected = item.assertions[key];
+      assert.equal(actual, expected, `Property ${key} should be ${expected}, was ${actual}`);
+    }
+  });
+});
