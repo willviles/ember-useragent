f7ce57a7b2a1bb61f8f4465cfaa569c6c4265496
Jan Buschtöns <jan.buschtoens@clark.de>
2018-07-29 12:16:58 +0200

    refactor

--- index.js
+++ index.js
@@ -1,62 +1,17 @@
 'use strict';
 
-const BroccoliDebug = require('broccoli-debug');
-const Funnel = require('broccoli-funnel');
-const mergeTrees = require('broccoli-merge-trees');
-const path = require('path');
-const fastbootTransform = require('fastboot-transform');
-
 module.exports = {
   name: 'ember-useragent',
 
   included(app) {
-    this.importDependencies(app);
-    return this._super.included.apply(this, arguments);
-  },
-
-  importDependencies(app) {
-    if (arguments.length < 1) {
-      throw new Error('Application instance must be passed to import');
-    }
-
-    app.import(this.treePaths.vendor + '/ua-parser-js/ua-parser.js');
-    
-    app.import('vendor/shims/ua-parser-js.js', {
-      exports: {
-        ['ua-parser-js']: ['default']
-      }
+    // https://github.com/rwjblue/ember-cli-cjs-transform/issues/23#issuecomment-408665511
+    const modulePath = require.resolve('ua-parser-js').match(/node_modules\/.*$/)[0];
+    app.import(modulePath, {
+      using: [
+        { transformation: 'cjs', as: 'ua-parser-js' }
+      ]
     });
 
-  },
-
-  treeForVendor(vendorTree) {
-    let debugTree = BroccoliDebug.buildDebugCallback(this.name),
-        trees = [];
-
-    if (vendorTree) {
-      trees.push(
-        debugTree(vendorTree, 'vendorTree')
-      );
-    }
-
-    let js = fastbootTransform(
-      moduleToFunnel('ua-parser-js')
-    );
-
-    trees.push(
-      debugTree(js, 'js')
-    );
-
-    return mergeTrees(trees);
+    return this._super.included.apply(this, arguments);
   }
 };
-
-function moduleToFunnel(moduleName, destination) {
-  return new Funnel(resolveModulePath(moduleName), {
-    destDir: destination || moduleName
-  });
-}
-
-function resolveModulePath(moduleName) {
-  return path.dirname(require.resolve(moduleName));
-}
