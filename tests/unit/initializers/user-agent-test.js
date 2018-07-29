23c79f498f3cda435a39a06c6433a828a8cc3464
Jan Buschtöns <jan.buschtoens@clark.de>
2018-07-29 12:41:41 +0200

    chore(tests): update to latest standards

--- tests/unit/initializers/user-agent-test.js
+++ tests/unit/initializers/user-agent-test.js
@@ -5,22 +5,23 @@ import { initialize } from 'dummy/initializers/user-agent';
 import { module, test } from 'qunit';
 import destroyApp from '../../helpers/destroy-app';
 
-module('Unit | Initializer | user-agent', {
-  beforeEach() {
+module('Unit | Initializer | user-agent', function(hooks) {
+  hooks.beforeEach(function() {
     run(() => {
       this.application = Application.create();
       this.application.deferReadiness();
     });
-  },
-  afterEach() {
+  });
+
+  hooks.afterEach(function() {
     destroyApp(this.application);
-  }
-});
+  });
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  initialize(this.application);
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    initialize(this.application);
 
-  // you would normally confirm the results of the initializer here
-  assert.ok(true);
+    // you would normally confirm the results of the initializer here
+    assert.ok(true);
+  });
 });
