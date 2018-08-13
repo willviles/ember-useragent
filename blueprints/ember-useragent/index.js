b33a225715391f5bc99f73e53c0137244ca8eb87
Will Viles <will@vil.es>
2018-08-13 16:32:14 +0100

    fix: adds ember-cli-cjs-transform addon to project in blueprint

--- blueprints/ember-useragent/index.js
+++ blueprints/ember-useragent/index.js
@@ -2,9 +2,17 @@
 'use strict';
 
 module.exports = {
-  normalizeEntityName: function() {
+  normalizeEntityName () {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
+  },
+
+  afterInstall () {
+    return this.addAddonsToProject({
+      packages: [
+        { name: 'ember-cli-cjs-transform' }
+      ]
+    })
   }
 };
