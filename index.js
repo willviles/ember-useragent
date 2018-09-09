bf5cd14a05d0db9230c6648ed7e305365871fa1d
Kalle Rutanen <kalle.rutanen@lightspeedgraphics.co.nz>
2018-09-09 18:26:00 +0300

    Fixed path-regex in index.js under Windows.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   included(app) {
     // https://github.com/rwjblue/ember-cli-cjs-transform/issues/23#issuecomment-408665511
-    const modulePath = require.resolve('ua-parser-js').match(/node_modules\/.*$/)[0];
+    const modulePath = require.resolve('ua-parser-js').match(/node_modules(\/|\\).*$/)[0];
     app.import(modulePath, {
       using: [
         { transformation: 'cjs', as: 'ua-parser-js' }
