7baaf7a395f27bb88a089f57b378f8812c34b993
Will Viles <will@vil.es>
2018-11-14 09:57:01 +0000

    Swaps ember-cli-cjs-transform for ember-auto-import

--- blueprints/ember-useragent/index.js
+++ blueprints/ember-useragent/index.js
@@ -6,13 +6,5 @@ module.exports = {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
     // to us
-  },
-
-  afterInstall () {
-    return this.addAddonsToProject({
-      packages: [
-        { name: 'ember-cli-cjs-transform' }
-      ]
-    })
   }
 };
--- index.js
+++ index.js
@@ -3,15 +3,9 @@
 module.exports = {
   name: 'ember-useragent',
 
-  included(app) {
-    // https://github.com/rwjblue/ember-cli-cjs-transform/issues/23#issuecomment-408665511
-    const modulePath = require.resolve('ua-parser-js').match(/node_modules(\/|\\).*$/)[0];
-    app.import(modulePath, {
-      using: [
-        { transformation: 'cjs', as: 'ua-parser-js' }
-      ]
-    });
-
-    return this._super.included.apply(this, arguments);
+  options: {
+    autoImport:{
+      exclude: ['ember-cli-babel']
+    }
   }
 };
